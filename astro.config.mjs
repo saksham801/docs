0b4c8c16c311792b606fe9c097617a2b35b62626
Saksham Dubey <119166625+saksham801@users.noreply.github.com>
2026-09-19 09:20:34 +0000

    Add components and custom CSS to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,12 @@ export default defineConfig({
         src: './src/assets/logo.svg',
         alt: 'SD — Saksham’s Docs',
       },
+      components: {
+        Header: './src/components/Header.astro',
+        Footer: './src/components/Footer.astro',
+        PageTitle: './src/components/PageTitle.astro',
+      },
+      customCss: ['./src/styles/docs.css'],
       lastUpdated: true,
       pagination: true,
       pagefind: true,
